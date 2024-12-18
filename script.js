52d9dca1b012cc05d554455a37a5e6c373e8b9e5
Nanatsunota1 <vitor_andrademanoel@hotmail.com>
2024-12-18 00:17:34 -0300

    adicionando

--- script.js
+++ script.js
@@ -103,51 +103,135 @@ window.addEventListener('resize', () => {
 animate();
 
 
-const slider = document.querySelector(".cards");
+const slider = document.getElementById("cards");
 
 // Adiciona o evento de scroll (wheel)
-slider.addEventListener("wheel", (e) => {
-    // Previne o comportamento padrão (como rolar a página)
-    e.preventDefault();
+var canvas = document.querySelector("#scene"),
+  ctx = canvas.getContext("2d"),
+  particles = [],
+  amount = 0,
+  mouse = {x:0,y:0},
+  radius = 1;
 
-    // Calcula o quanto o slider deve mover com base no scroll
-    const scrollAmount = e.deltaY; // deltaY é o valor do movimento do mouse (positivo para baixo, negativo para cima)
-    
-    // Ajusta o scroll do slider de acordo com o valor do scroll
-    slider.scrollLeft += scrollAmount;
-});
+var colors = ["#468966","#FFF0A5", "#FFB03B","#B64926", "#8E2800"];
 
-slider.addEventListener("mousedown", (e) => {
-  // Verifica se o botão pressionado é o do meio (scroll)
-  if (e.button === 1) {
-      isDown = true;
-      slider.classList.add("active");
-      startX = e.pageX - slider.offsetLeft;
-      scrollLeft = slider.scrollLeft;
 
-      // Evita o comportamento padrão do navegador ao pressionar o botão de scroll (como o "auto-scroll")
-      e.preventDefault();
+
+var ww = 1500;
+var wh = 1500;
+
+function Particle(x,y){
+  this.x =  Math.random()*ww;
+  this.y =  Math.random()*wh;
+  this.dest = {
+    x : x,
+    y: y
+  };
+  this.r =  Math.random()*5 + 2;
+  this.vx = (Math.random()-0.5)*20;
+  this.vy = (Math.random()-0.5)*20;
+  this.accX = 0;
+  this.accY = 0;
+  this.friction = Math.random()*0.05 + 0.94;
+
+  this.color = colors[Math.floor(Math.random()*6)];
+}
+
+Particle.prototype.render = function() {
+
+
+  this.accX = (this.dest.x - this.x)/600;
+  this.accY = (this.dest.y - this.y)/600;
+  this.vx += this.accX;
+  this.vy += this.accY;
+  this.vx *= this.friction;
+  this.vy *= this.friction;
+
+  this.x += this.vx;
+  this.y +=  this.vy;
+
+  ctx.fillStyle = this.color;
+  ctx.beginPath();
+  ctx.arc(this.x, this.y, this.r, Math.PI * 2, false);
+  ctx.fill();
+
+  var a = this.x - mouse.x;
+  var b = this.y - mouse.y;
+
+  var distance = Math.sqrt( a*a + b*b );
+  if(distance<(radius*70)){
+    this.accX = (this.x - mouse.x)/100;
+    this.accY = (this.y - mouse.y)/100;
+    this.vx += this.accX;
+    this.vy += this.accY;
   }
-});
 
-// Quando o botão do scroll é solto
-slider.addEventListener("mouseup", () => {
-  isDown = false;
-  slider.classList.remove("active");
-});
+}
 
-// Quando o cursor sai da área do slider
-slider.addEventListener("mouseleave", () => {
-  isDown = false;
-  slider.classList.remove("active");
-});
+function onMouseMove(e){
+  mouse.x = e.clientX;
+  mouse.y = e.clientY;
+}
 
-// Movimento do slider enquanto o botão de scroll está pressionado
-slider.addEventListener("mousemove", (e) => {
-  if (!isDown) return;
-  e.preventDefault();
+function onTouchMove(e){
+  if(e.touches.length > 0 ){
+    mouse.x = e.touches[0].clientX;
+    mouse.y = e.touches[0].clientY;
+  }
+}
 
-  const x = e.pageX - slider.offsetLeft;
-  const walk = (x - startX) * 3; // A velocidade do scroll horizontal
-  slider.scrollLeft = scrollLeft - walk;
-});
+function onTouchEnd(e){
+mouse.x = -9999;
+mouse.y = -9999;
+}
+
+function initScene(){
+  ww = canvas.width = window.innerWidth;
+  wh = canvas.height = window.innerHeight;
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  ctx.font = "bold "+(ww/10)+"px sans-serif";
+  ctx.textAlign = "center";
+  ctx.fillText("FAQ", ww/2, wh/2);
+
+  var data  = ctx.getImageData(0, 0, ww, wh).data;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.globalCompositeOperation = "screen";
+
+  particles = [];
+  for(var i=0;i<ww;i+=Math.round(ww/150)){
+    for(var j=0;j<wh;j+=Math.round(ww/150)){
+      if(data[ ((i + j*ww)*4) + 3] > 150){
+        particles.push(new Particle(i,j));
+      }
+    }
+  }
+  amount = particles.length;
+
+}
+
+function onMouseClick(){
+  radius++;
+  if(radius ===5){
+    radius = 0;
+  }
+}
+
+function render(a) {
+  requestAnimationFrame(render);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (var i = 0; i < amount; i++) {
+    particles[i].render();
+  }
+};
+
+
+window.addEventListener("resize", initScene);
+window.addEventListener("mousemove", onMouseMove);
+window.addEventListener("touchmove", onTouchMove);
+window.addEventListener("click", onMouseClick);
+window.addEventListener("touchend", onTouchEnd);
+initScene();
+requestAnimationFrame(render);
+
